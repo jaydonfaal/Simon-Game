e7ce76858787c24bda328e2bd3ea415e390b61c1
jaydonfaal <jaydonfaal@gmail.com>
2023-02-10 13:38:04 -0500

    Working
    Stylistic changes needed but game is functioning.

--- main.js
+++ main.js
@@ -4,6 +4,7 @@ const blue = document.getElementById('blue');
 const red = document.getElementById('red');
 const id = document.getElementById('level');
 const dir = document.getElementById('directions');
+const timer = ms => new Promise(res => setTimeout(res, ms));
 dir.textContent = ("Press Enter Key To Start");
 id.textContent = "0";
 
@@ -15,7 +16,7 @@ let userInput = [];
 let count = 0;
 let userCount = 0;
 
-console.log(pattern);
+
 
 (document).addEventListener('keyup', e => {
   if (event.keyCode === 13) {
@@ -31,7 +32,8 @@ console.log(pattern);
 
 yellow.addEventListener('click', e => {
   if(userCanPlay == true){
-    console.log("yellow clicked");
+    yellow.classList.add('glow');
+    setTimeout(function()   {  yellow.classList.remove('glow'); }, 400);
       userInput.push(0);
       checkInput(0);
   }
@@ -39,6 +41,8 @@ yellow.addEventListener('click', e => {
 });
  green.addEventListener('click', e =>{
    if(userCanPlay == true){
+     green.classList.add('glow');
+     setTimeout(function()   {  green.classList.remove('glow'); }, 400);
      console.log("green clicked");
   userInput.push(1);
   checkInput(1);
@@ -46,6 +50,8 @@ yellow.addEventListener('click', e => {
  });
  blue.addEventListener('click', e => {
    if(userCanPlay == true){
+     blue.classList.add('glow');
+     setTimeout(function()   {  blue.classList.remove('glow'); }, 400);
   console.log("blue clicked");
     userInput.push(2);
     checkInput(2);
@@ -53,6 +59,8 @@ yellow.addEventListener('click', e => {
  });
  red.addEventListener('click', e =>{
    if(userCanPlay == true){
+     red.classList.add('glow');
+     setTimeout(function()   {  red.classList.remove('glow'); }, 400);
   console.log("clicked");
   userInput.push(3);
   checkInput(3);
@@ -62,38 +70,37 @@ yellow.addEventListener('click', e => {
 
 function getRandomInt() {
     pattern.push(Math.floor(Math.random() * 4));
-    count++;
     displayPattern();
 }
 
-function displayPattern(){
-  userCanPlay = false;
-  for(let i = 0; i < count; i++){
-
-setTimeout(() => {
-  switch (pattern[i]){
+async function displayPattern(){
+  for(let i = 0; i < pattern.length; i++){
+    await timer(1000);
+    switch (pattern[i]){
     case 0:
-    yellow.classList.add('clicked');
-    setTimeout(() => {  yellow.classList.toggle('clicked'); }, 500);
+    yellow.classList.add('glow');
+    setTimeout(function()  {  yellow.classList.remove('glow'); }, 400);
     break;
     case 1:
-    green.classList.add('clicked');
-    setTimeout(() => {  green.classList.toggle('clicked'); }, 500);
+    green.classList.add('glow');
+    setTimeout(function()  {  green.classList.remove('glow'); }, 400);
     break;
     case 2:
-    blue.classList.add('clicked');
-    setTimeout(() => {  blue.classList.toggle('clicked'); }, 500);
+    blue.classList.add('glow');
+    setTimeout(function()   {  blue.classList.remove('glow'); }, 400);
     break;
     case 3:
-    red.classList.add('clicked');
-    setTimeout(() => {  red.classList.toggle('clicked'); }, 500);
+    red.classList.add('glow');
+    setTimeout(function()  {  red.classList.remove('glow'); }, 400);
     break;
 }
-},1000)
+
 
   }
   userCanPlay = true;
 }
+
+
 
 function gameOver(){
   gameInAction = false;
@@ -114,16 +121,7 @@ function checkInput(input){
 
     id.textContent = userCount;
     userCount = 0;
-    setTimeout(() =>{getRandomInt();},1000)
+    userCanPlay = false;
+    setTimeout(() =>{getRandomInt();}, 1000)
   }
 }
-
-// function gatherUserInput(){
-//   for(let i = 0; i < count; i++){
-//     if(userInput[i] != pattern[i]){
-//       gameOver();
-//     }
-//
-//   }
-//
-//}
