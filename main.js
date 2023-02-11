cbdeecec25fa8649746a314456365fe500544263
jaydonfaal <79113964+jaydonfaal@users.noreply.github.com>
2023-02-10 20:44:32 -0800

    Update main.js

--- main.js
+++ main.js
@@ -9,21 +9,33 @@ const header = document.getElementById('header');
 const title = document.getElementById('title');
 const letters = ["S", "I", "M", "O", "N"];
 const timer = ms => new Promise(res => setTimeout(res, ms));
+
 dir.textContent = ("Press Enter Key To Start");
 level.textContent = "0";
-
 
 let gameInAction = false;
 let userCanPlay = false;
 let pattern = [];
 let colors = [yellow, red, blue, green];
 let userInput = [];
-let count = 0;
 let userCount = 0;
 
-
-
 addTitle();
+
+//functions
+
+function gameOver(){
+  gameInAction = false;
+  userCanPlay = false;
+  userCount = 0;
+  console.log(userCount);
+  pattern.length = 0;
+  userInput.length = 0;
+  level.textContent = 0;
+  game_over.textContent = ("GAME OVER");
+  dir.textContent = ("Press Enter Key To Start");
+}
+
 async function addTitle(){
   for (let i = 0; i < 5; i++) {
     await timer(800)
@@ -33,64 +45,6 @@ async function addTitle(){
       header.append(letterElement);
   }
 }
-
-
-
-
-
-
-
-
-(document).addEventListener('keyup', e => {
-  if (event.keyCode === 13) {
-    if(gameInAction == false){
-      game_over.textContent = "";
-      setTimeout(() => {
-        getRandomInt();
-      },1000);
-    }
-    gameInAction = true;
-    dir.textContent = "";
-}
-});
-
-yellow.addEventListener('click', e => {
-  if(userCanPlay == true){
-    yellow.classList.add('glow');
-    setTimeout(function()   {  yellow.classList.remove('glow'); }, 400);
-      userInput.push(0);
-      checkInput(0);
-  }
-
-});
- green.addEventListener('click', e =>{
-   if(userCanPlay == true){
-     green.classList.add('glow');
-     setTimeout(function()   {  green.classList.remove('glow'); }, 400);
-     console.log("green clicked");
-  userInput.push(1);
-  checkInput(1);
-}
- });
- blue.addEventListener('click', e => {
-   if(userCanPlay == true){
-     blue.classList.add('glow');
-     setTimeout(function()   {  blue.classList.remove('glow'); }, 400);
-  console.log("blue clicked");
-    userInput.push(2);
-    checkInput(2);
-  }
- });
- red.addEventListener('click', e =>{
-   if(userCanPlay == true){
-     red.classList.add('glow');
-     setTimeout(function()   {  red.classList.remove('glow'); }, 400);
-  console.log("clicked");
-  userInput.push(3);
-  checkInput(3);
-}
- });
-
 
 function getRandomInt() {
     pattern.push(Math.floor(Math.random() * 4));
@@ -103,7 +57,7 @@ async function displayPattern(){
     switch (pattern[i]){
     case 0:
     yellow.classList.add('glow');
-    setTimeout(function()  {  yellow.classList.remove('glow'); }, 400);
+    setTimeout(function()  { yellow.classList.remove('glow'); }, 400);
     break;
     case 1:
     green.classList.add('glow');
@@ -118,36 +72,94 @@ async function displayPattern(){
     setTimeout(function()  {  red.classList.remove('glow'); }, 400);
     break;
 }
-
-
   }
   userCanPlay = true;
 }
 
+// function checkInput(input){
+//   console.log(userCount);
+//   if(input != pattern[userCount] && userCanPlay == true){
+//
+//     console.log(userCount +"p "+pattern.length + " " + input);
+//     gameOver();
+//   }
+//   userCount++;
+//   if (pattern.length == userCount) {
+//
+//     level.textContent = userCount;
+//     userCount = 0;
+//     userCanPlay = false;
+//     setTimeout(() =>{getRandomInt();}, 1000)
+//   }
+// }
 
 
-function gameOver(){
-  gameInAction = false;
-  userCanPlay = false;
-  userCount = 0;
-  pattern = [];
-  userInput = [];
-  count = [];
-  level.textContent = 0;
-  game_over.textContent = ("GAME OVER");
-  dir.textContent = ("Press Enter Key To Start");
-}
 
 function checkInput(input){
-  if(input != pattern[userCount] && userCanPlay == true){
+  if(input == pattern[userCount] && userCanPlay == true && gameInAction == true){
+    userCount++;
+    if(userCount == pattern.length){
+      level.textContent = userCount;
+      userCount = 0;
+      getRandomInt();
+    }
+  }
+  else{
     gameOver();
-  }
-  userCount++;
-  if (pattern.length == userCount) {
 
-    level.textContent = userCount;
-    userCount = 0;
-    userCanPlay = false;
-    setTimeout(() =>{getRandomInt();}, 1000)
   }
+
 }
+
+
+
+//Event Listeners
+
+
+(document).addEventListener('keyup', e => {
+  if (event.keyCode === 13) {
+    if(gameInAction == false){
+      game_over.textContent = "";
+      setTimeout(() => {
+        getRandomInt();
+      },1000);
+    }
+    gameInAction = true;
+      dir.textContent = "";
+}
+});
+
+yellow.addEventListener('click', e => {
+  if(userCanPlay == true){
+    yellow.classList.add('glow');
+    setTimeout(function()   {  yellow.classList.remove('glow'); }, 400);
+      checkInput(0);
+  }
+});
+
+ green.addEventListener('click', e =>{
+   if(userCanPlay == true){
+     green.classList.add('glow');
+     setTimeout(function()   {  green.classList.remove('glow'); }, 400);
+     console.log("green clicked");
+  checkInput(1);
+}
+});
+
+ blue.addEventListener('click', e => {
+   if(userCanPlay == true){
+     blue.classList.add('glow');
+     setTimeout(function()   {  blue.classList.remove('glow'); }, 400);
+  console.log("blue clicked");
+    checkInput(2);
+  }
+ });
+
+ red.addEventListener('click', e =>{
+   if(userCanPlay == true){
+     red.classList.add('glow');
+     setTimeout(function()   {  red.classList.remove('glow'); }, 400);
+  console.log("clicked");
+  checkInput(3);
+ }
+});
